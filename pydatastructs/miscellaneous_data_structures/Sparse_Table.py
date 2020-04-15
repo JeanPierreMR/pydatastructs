0465eaefcf44b19299088d7ebc18cbc4c80fb279
Adriana Mundo <adrianamundog@hotmail.com>
2020-04-15 17:44:27 -0600

    Working #25
    Functions working.

--- pydatastructs/miscellaneous_data_structures/Sparse_Table.py
+++ pydatastructs/miscellaneous_data_structures/Sparse_Table.py
@@ -1,28 +1,49 @@
 import math
-class Sparse_Table:
-    def sparsetableConstruction(self, arr):
-        '''
+
+def sparsetableConstruction(arr):
+
+    '''
     Notes about sparse table:
     The idea is to precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
     '''
+
     length = len(arr)
     sparsetable = [[-1 for i in range(length)]
     for j in range(int(math.log(length,2))+1)]
     
     for j in range(int(math.log(length, 2))+1):
         for i in range(length):
-            minimum = i
+            minimum = i 
             #2^n (2)
             if (i+(2**j)-1 < length):
-                for a in range(i, i+(2**j-1)):
+                for x in range(i, i+(2**j-1)):
                     if (array[x] < array[minimum]): 
-                        minimum = a
+                        minimum = x
                 
                 sparsetable[j][i] = minimum 
     
     return sparsetable
 
+def queryConstruction(arr, sparsetable, range):
+    lengtharr = len(arr)
+    
+    minimumel = []
+
+    while lengtharr > 0:
+        b = int(math.log(len(arr),2))
+        minimumel.append(arr[sparsetable[b][range[0]]])
+        lengtharr = lengtharr - (2**b)
+    
+    return min(minimumel)
+
+if __name__ == '__main__':
+    array = [5, 7, 4, 3, 8, 9]
+
+    sparsetable = sparsetableConstruction(array)
+    qlist = [[3, 5], [1, 6], [0, 2]]
 
 
-def query():
+    for x in qlist:
+        print(queryConstruction(array, sparsetable, x))
+    
 
