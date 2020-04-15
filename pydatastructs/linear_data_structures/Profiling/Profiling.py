8711daba11fab2b0a31313bea4a299be310089dc
JeanPierreMR <jpmejicanos@ufm.edu>
2020-04-15 00:19:49 -0600

    Create Profiling.py
    Adding profiling for multi dimensional array

---
+++ pydatastructs/linear_data_structures/Profiling/Profiling.py
@@ -0,0 +1,24 @@
+from pydatastructs.linear_data_structures import (
+    OneDimensionalArray, DynamicOneDimensionalArray, MultiDimensionalArray)
+from pydatastructs.utils.raises_util import raises
+from random import randint
+import cProfile
+
+def test_MultiDimensionalArray():
+    MDA = MultiDimensionalArray
+    i = 200
+    j = 5
+    while j >= 0:
+        size = 20
+        A = MDA(int, size, size, size, size)
+        A.fill(randint(-20000, 20000))
+        while i >= 0:
+            A[randint(0,19)][randint(0,19)][randint(0,19)][randint(0,19)] = randint(-20000, 20000)
+            i -= 1
+        j -= 1
+
+def main():
+    print(cProfile.run("test_MultiDimensionalArray()"))
+
+if __name__ == "__main__":
+    main()
