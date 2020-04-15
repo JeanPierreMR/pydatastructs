d7dbbcee5e9296c1b11ee3e7b36d83afb3d749fb
Adriana Mundo <adrianamundog@hotmail.com>
2020-04-15 16:43:28 -0600

    Function #25
    First function builder.

--- pydatastructs/miscellaneous_data_structures/Sparse_Table.py
+++ pydatastructs/miscellaneous_data_structures/Sparse_Table.py
@@ -1,7 +1,27 @@
 import math
 
 def sparsetableConstruction(arr):
+    '''
+    Notes about sparse table:
+    The idea is to precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
+    '''
     length = len(arr)
     sparsetable = [[-1 for i in range(length)]
     for j in range(int(math.log(length,2))+1)
     ]
+
+    for j in range(int(math.log(length, 2))+1):
+        for i in range(length):
+            minimum = i
+            if (i+(2**j)-1 < length):
+                for a in range(i, i+(2**j-1)):
+                    if (array[x] < array[minimum]): 
+                        minimum = a
+                
+                sparsetable[j][i] = minimum 
+    
+    return sparsetable
+
+
+
+def query():
