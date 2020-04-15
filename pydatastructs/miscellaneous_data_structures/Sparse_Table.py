a834b5f48b3bcfcd40923f8ac7db2e63bc91c14f
JeanPierreMR <jpmejicanos@ufm.edu>
2020-04-15 17:54:41 -0600

    Update Sparse_Table.py
    Making it a class

--- pydatastructs/miscellaneous_data_structures/Sparse_Table.py
+++ pydatastructs/miscellaneous_data_structures/Sparse_Table.py
@@ -1,49 +1,45 @@
 import math
 
-def sparsetableConstruction(arr):
+class SparseTable():
+    def __init__(self, arr):
+        '''
+            Notes about sparse table:
+            The idea is to precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
+            '''
 
-    '''
-    Notes about sparse table:
-    The idea is to precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
-    '''
+        length = len(arr)
+        sparsetable = [[-1 for i in range(length)]
+                       for j in range(int(math.log(length, 2)) + 1)]
 
-    length = len(arr)
-    sparsetable = [[-1 for i in range(length)]
-    for j in range(int(math.log(length,2))+1)]
-    
-    for j in range(int(math.log(length, 2))+1):
-        for i in range(length):
-            minimum = i 
-            #2^n (2)
-            if (i+(2**j)-1 < length):
-                for x in range(i, i+(2**j-1)):
-                    if (array[x] < array[minimum]): 
-                        minimum = x
-                
-                sparsetable[j][i] = minimum 
-    
-    return sparsetable
+        for j in range(int(math.log(length, 2)) + 1):
+            for i in range(length):
+                minimum = i
+                # 2^n (2)
+                if (i + (2 ** j) - 1 < length):
+                    for x in range(i, i + (2 ** j - 1)):
+                        if (array[x] < array[minimum]):
+                            minimum = x
 
-def queryConstruction(arr, sparsetable, range):
-    lengtharr = len(arr)
-    
-    minimumel = []
+                    sparsetable[j][i] = minimum
+        self._data = sparsetable
 
-    while lengtharr > 0:
-        b = int(math.log(len(arr),2))
-        minimumel.append(arr[sparsetable[b][range[0]]])
-        lengtharr = lengtharr - (2**b)
-    
-    return min(minimumel)
+    def query(self, arr, range):
+        lengtharr = len(arr)
+        minimumel = []
+
+        while lengtharr > 0:
+            b = int(math.log(len(arr),2))
+            minimumel.append(arr[self._data[b][range[0]]])
+            lengtharr = lengtharr - (2**b)
+
+        return min(minimumel)
 
 if __name__ == '__main__':
     array = [5, 7, 4, 3, 8, 9]
 
-    sparsetable = sparsetableConstruction(array)
+    sparsetable = SparseTable(array)
     qlist = [[3, 5], [1, 6], [0, 2]]
-
-
     for x in qlist:
-        print(queryConstruction(array, sparsetable, x))
+        print(sparsetable.query(array, x))
     
 
