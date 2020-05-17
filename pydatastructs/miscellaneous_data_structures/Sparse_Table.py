555757ce3b822bb3ab95ce439ed45ca615904fa0
Adriana Mundo <adrianamundog@hotmail.com>
2020-05-16 19:57:40 -0600

    Code working

--- pydatastructs/miscellaneous_data_structures/Sparse_Table.py
+++ pydatastructs/miscellaneous_data_structures/Sparse_Table.py
@@ -1,49 +1,74 @@
-import math
+import math 
+from math import log2 #importing log2 utilized for constructing a sparse table
+#sparse table: precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
+#Parts of sparse table: construction, query and range minimum query
 
-def sparsetableConstruction(arr):
+Max = 500
 
-    '''
-    Notes about sparse table:
-    The idea is to precompute minimum of all subarrays of size 2j where j varies from 0 to Log n.
-    '''
-
-    length = len(arr)
-    sparsetable = [[-1 for i in range(length)]
-    for j in range(int(math.log(length,2))+1)]
-    
-    for j in range(int(math.log(length, 2))+1):
-        for i in range(length):
-            minimum = i 
-            #2^n (2)
-            if (i+(2**j)-1 < length):
-                for x in range(i, i+(2**j-1)):
-                    if (array[x] < array[minimum]): 
-                        minimum = x
-                
-                sparsetable[j][i] = minimum 
-    
-    return sparsetable
-
-def queryConstruction(arr, sparsetable, range):
-    lengtharr = len(arr)
-    
-    minimumel = []
-
-    while lengtharr > 0:
-        b = int(math.log(len(arr),2))
-        minimumel.append(arr[sparsetable[b][range[0]]])
-        lengtharr = lengtharr - (2**b)
-    
-    return min(minimumel)
-
-if __name__ == '__main__':
-    array = [5, 7, 4, 3, 8, 9]
-
-    sparsetable = sparsetableConstruction(array)
-    qlist = [[3, 5], [1, 6], [0, 2]]
+table = [[0 for i in range(500)] 
+			for j in range(500)] 
 
 
-    for x in qlist:
-        print(queryConstruction(array, sparsetable, x))
+class Query: 
+	def __init__(self, a, b): 
+		self.A = a
+		self.B = b
+
+def sparsetableconstruction(arr: list, n: int): 
+	global table
+
+	for i in range(n): 
+		table[i][0] = i 
+
+	j = 1 #little to bigger intervals
+	while (1 << j) <= n: 
+
+		# Compute minimum value for all intervals with size 2^j 
+		i = 0
+		while i + (1 << j) - 1 < n: 
+
+			#comparition
+			if (arr[table[i][j - 1]] < 
+				arr[table[i + (1 << (j - 1))][j - 1]]): 
+				table[i][j] = table[i][j - 1] 
+			else: 
+				table[i][j] = table[i +
+							(1 << (j - 1))][j - 1] 
+
+			i += 1  #adding 1 for each
+		j += 1
+
+def query(arr: list, A: int, B: int) -> int: 
+	global table
+
+	j = int(log2(B - A + 1)) #example:floor(Log2(10-2+1)) = 3
+
+	#comparition again
+	if (arr[table[A][j]] <=
+		arr[table[B - (1 << j) + 1][j]]): 
+		return arr[table[A][j]] 
+	else: 
+		return arr[table[B - (1 << j) + 1][j]] 
+ 
+def rmq(arr: list, x: int, q: list, y: int): 
+
+	sparsetableconstruction(arr, x) 
+
+	#sum queries
+	for i in range(y): 
+ 
+		A = q[i].A
+		B = q[i].B
+ 
+		print("The minimum of [%d, %d] is %d" %(A, B, query(arr, A, B))) 
+
+# Driver Code 
+if __name__ == "__main__": 
+	a = [4, 6, 1, 5, 7, 3] 
+	x = len(a) 
+	q = [Query(0, 2), Query(2, 4), Query(3, 5)] 
+	y = len(q) 
+
+	rmq(a, x, q, y) 
     
 
